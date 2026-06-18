c24337aecf4103821450247d229d4b77e31659b8
SiddhiikaN <siddhikanagarkar@gmail.com>
2026-06-18 19:46:31 +0530

    add LoadConfig class

--- src/main/java/com/siddhika/apiloadgen/config/LoadConfig.java
+++ src/main/java/com/siddhika/apiloadgen/config/LoadConfig.java
@@ -1,5 +1,32 @@
 package com.siddhika.apiloadgen.config;
 
 public class LoadConfig {
+
+    private String targetUrl;
+    private int threads;
+    private int durationSeconds;
+    private int requestsPerSecond;
+
+    public LoadConfig(String targetUrl, int threads, int durationSeconds, int requestsPerSecond){
+        this.targetUrl = targetUrl;
+        this.threads = threads;
+        this.durationSeconds = durationSeconds;
+        this.requestsPerSecond = requestsPerSecond;
+    }
+
+    public String getTargetUrl(){
+        return targetUrl;
+    }
+
+    public int getThreads(){
+        return threads;
+    }
+    public int getDurationSeconds(){
+        return durationSeconds;
+    }
+
+    public int getRequestsPerSecond(){
+        return requestsPerSecond;
+    }
     
 }
