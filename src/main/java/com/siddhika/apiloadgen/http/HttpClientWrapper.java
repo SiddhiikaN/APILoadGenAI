6ea283832694d8886cd171a1974605639eae11af
SiddhiikaN <siddhikanagarkar@gmail.com>
2026-06-19 12:59:31 +0530

    add HttpClientWrapper class

--- src/main/java/com/siddhika/apiloadgen/http/HttpClientWrapper.java
+++ src/main/java/com/siddhika/apiloadgen/http/HttpClientWrapper.java
@@ -1,5 +1,28 @@
 package com.siddhika.apiloadgen.http;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 
 public class HttpClientWrapper {
+    private final HttpClient client;
+
+    public HttpClientWrapper(){
+        this.client = HttpClient.newBuilder()
+        .connectTimeout(Duration.ofSeconds(5))
+        .build();
+    }
+
+    public HttpResponse<String> post(String url, String body) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(url))
+        .header("Content-Type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(body))
+        .timeout(Duration.ofSeconds(10))
+        .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
     
 }
