6bf99c2fa420e30d5d29c42642980e65cbd75f71
SiddhiikaN <siddhikanagarkar@gmail.com>
2026-06-22 20:36:21 +0530

    add ReportPrinter class

--- src/main/java/com/siddhika/apiloadgen/report/ReportPrinter.java
+++ src/main/java/com/siddhika/apiloadgen/report/ReportPrinter.java
@@ -1,5 +1,24 @@
 package com.siddhika.apiloadgen.report;
 
+import com.siddhika.apiloadgen.engine.MetricsCollector;
+
 public class ReportPrinter {
+    private final MetricsCollector metrics;
+
+    public ReportPrinter(MetricsCollector metrics) {
+        this.metrics = metrics;
+    }
+
+    public void print() {
+        System.out.println("\n========== LOAD TEST REPORT ==========");
+        System.out.println("Total requests  : " + metrics.getTotalRequests());
+        System.out.println("Sucess          : " + metrics.getSuccessCount());
+        System.out.println("Error           : " + metrics.getErrorCount());
+        System.out.println("Avg Latency     :" + metrics.getAverageLatency() + " ms");
+        System.out.println("p50 Latency     :" + metrics.getPercentile(50) + " ms");
+        System.out.println("p95 Latency     :" + metrics.getPercentile(95) + " ms");
+        System.out.println("p99 Latency     :" + metrics.getPercentile(99) + " ms");
+        System.out.println("=====================================\n");
+    }
     
 }
