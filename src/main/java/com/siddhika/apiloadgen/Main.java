d92411deef90e7ffb3340dd33f76c62c8c7f091b
SiddhiikaN <siddhikanagarkar@gmail.com>
2026-06-25 19:28:22 +0530

    add Main class

--- src/main/java/com/siddhika/apiloadgen/Main.java
+++ src/main/java/com/siddhika/apiloadgen/Main.java
@@ -1,9 +1,30 @@
 package com.siddhika.apiloadgen;
 
+import com.siddhika.apiloadgen.config.LoadConfig;
+import com.siddhika.apiloadgen.engine.LoadRunner;
+import com.siddhika.apiloadgen.engine.ChaosGenerator;
+import com.siddhika.apiloadgen.engine.MetricsCollector;
+import com.siddhika.apiloadgen.report.ReportPrinter;
+import com.siddhika.apiloadgen.report.AIAnalyser;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("APILoadGenAI starting...");
+    public static void main(String[] args) throws Exception {
+        String url = "http://localhost:8080/api/test";
+        int threads = 10;
+        int duration = 30;
+        int rps = 5;
+
+        LoadConfig config = new LoadConfig(url, threads, duration, rps);
+        MetricsCollector metrics = new MetricsCollector();
+        ChaosGenerator chaos = new ChaosGenerator();
+        LoadRunner runner = new LoadRunner(config, metrics, chaos);
+
+        System.out.println("Starting load test against: " + url);
+        runner.run();
+
+        new ReportPrinter(metrics).print();
+        new AIAnalyser(metrics).analyse();
     }
     
 }
