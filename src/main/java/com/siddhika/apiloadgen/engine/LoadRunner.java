9ba5ba5c1f2cb03b8ba6bddf837ecd529767da8d
SiddhiikaN <siddhikanagarkar@gmail.com>
2026-08-10 20:42:33 +0530

    fix: correct success threshold from <500 to <300

--- src/main/java/com/siddhika/apiloadgen/engine/LoadRunner.java
+++ src/main/java/com/siddhika/apiloadgen/engine/LoadRunner.java
@@ -7,13 +7,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-
 public class LoadRunner {
     private final LoadConfig config;
     private final MetricsCollector metrics;
     private final ChaosGenerator chaosGenerator;
 
-    public LoadRunner(LoadConfig config, MetricsCollector metrics, ChaosGenerator chaosGenerator){
+    public LoadRunner(LoadConfig config, MetricsCollector metrics, ChaosGenerator chaosGenerator) {
         this.config = config;
         this.metrics = metrics;
         this.chaosGenerator = chaosGenerator;
@@ -24,16 +23,16 @@ public class LoadRunner {
         ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
         long endTime = System.currentTimeMillis() + (config.getDurationSeconds()) * 1000L;
 
-        while(System.currentTimeMillis() < endTime) {
+        while (System.currentTimeMillis() < endTime) {
             executor.submit(() -> {
                 HttpClientWrapper http = new HttpClientWrapper();
                 String payLoad = chaosGenerator.generate();
                 long start = System.currentTimeMillis();
-                try{
+                try {
                     var response = http.post(config.getTargetUrl(), payLoad);
                     long latency = System.currentTimeMillis() - start;
-                    boolean success = response.statusCode() >= 200 && response.statusCode() < 500;
-                    metrics.record(success , latency);
+                    boolean success = response.statusCode() >= 200 && response.statusCode() < 300;
+                    metrics.record(success, latency);
                 } catch (Exception e) {
                     long latency = System.currentTimeMillis() - start;
                     metrics.record(false, latency);
@@ -41,12 +40,12 @@ public class LoadRunner {
             });
 
             Thread.sleep(1000L / config.getRequestsPerSecond());
-            
+
         }
 
         executor.shutdown();
         executor.awaitTermination(30, TimeUnit.SECONDS);
 
     }
-    
+
 }
